d249d943e73470c17961ef4d65929343dba56a0e
unknown <oliveiraabreu171@gmail.com>
2025-04-17 21:46:21 -0300

    criacao de tarefa

--- project/todos/views.py
+++ project/todos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Todo
+from django.views.generic import ListView,CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class TodoListView(ListView):
+    model = Todo;
+
+
+class TodoCreateView(CreateView):
+    model = Todo;
+    fields = ["title", "deadline"];
+    success_url = reverse_lazy("todo_list");
+
